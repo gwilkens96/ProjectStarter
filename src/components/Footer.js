caafa64749eba5d0e207010ef8fbf6f8b4481097
GriffinWilkens96 <gwilkens1234@gmail.com>
2022-01-10 15:45:26 -0500

    colorsss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function Footer() {
   return (
-    <div class="row align-items-center">
+    <div class="row align-items-center" style={{ backgroundColor: "#7D7461" }}>
       <div class="col">
-        Contact Griffin:
-        <ul style={{ listStyle: "none" }}>
+        <h4 style={{ color: "#f7f3e3" }}>Contact:</h4>
+        <ul style={{ listStyle: "none", color: "#f7f3e3" }}>
           <li>
             <a
               style={{ textDecoration: "none", color: "inherit" }}
               href="mailto: gbwilkens96@protonmail.com"
             >
-              gbwilkens96@protonmail.com 📧
+              gbwilkens96@protonmail.com
             </a>
           </li>
           <li>
@@ -19,14 +19,14 @@ function Footer() {
               style={{ textDecoration: "none", color: "inherit" }}
               href="tel:4044038524"
             >
-              404-403-8524 📱
+              404-403-8524
             </a>
           </li>
         </ul>
       </div>
       <div class="col">
-        Quick Links:
-        <ul style={{ listStyle: "none" }}>
+        <h4 style={{ color: "#f7f3e3" }}>Quick Links:</h4>
+        <ul style={{ listStyle: "none", color: "#f7f3e3" }}>
           <li>
             <a
               style={{ textDecoration: "none", color: "inherit" }}
@@ -35,7 +35,7 @@ function Footer() {
               rel="noreferrer"
             >
               {" "}
-              Linkedin 🔗
+              Linkedin
             </a>
           </li>
           <li>
@@ -45,16 +45,18 @@ function Footer() {
               target="_blank"
               rel="noreferrer"
             >
-              Github 🖥
+              Github
             </a>
           </li>
           <li>Portfolio</li>
         </ul>
       </div>
       <div class="col">
-        Griffin is a Georgia Tech alumnus with certification in full stack web
-        development. Thanks for choosing Griffin for your web dev needs. Let's
-        get this fire started!
+        <h4 style={{ color: "#f7f3e3" }}>
+          Griffin is a Georgia Tech alumnus with certification in full stack web
+          development. Thanks for choosing Griffin for your web dev needs. Let's
+          get this fire started!
+        </h4>
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <h1>FireStarter</h1>
+    <div style={{ backgroundColor: "white" }}>
+      <h1 style={{ color: "#6F1A07" }}>FireStarter</h1>
+      <h3 style={{ color: "#ff6f59" }}>
+        Let's put some fire under your project
+      </h3>
     </div>
   );
 }
