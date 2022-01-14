4d0eca20cffc6b24cdb5ccb8539d3fab753d4a08
GriffinWilkens96 <gwilkens1234@gmail.com>
2022-01-13 19:44:07 -0500

    login but must fix pathing to home

--- server/server.js
+++ server/server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.use(cors());
+app.use("/login", (req, res) => {
+  res.send({
+    token: "test123",
+  });
+});
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
--- src/components/Starter.js
+++ src/components/Starter.js
@@ -1,22 +1,27 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Profiles from "./Profiles";
 import Edit from "./Edits";
 import Forms from "./Forms";
+import Login from "./Login/Login";
 
 function StarterContainer() {
+  const [token, setToken] = useState();
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
   return (
     <div>
       <Header></Header>
-      <Navbar />
-      <Routes>
+      <BrowserRouter>
+        <Navbar />
         <Route exact path="/" element={<Profiles />} />
         <Route path="/edit/:id" element={<Edit />} />
         <Route path="/create" element={<Forms />} />
-      </Routes>
+      </BrowserRouter>
       <Footer></Footer>
     </div>
   );
